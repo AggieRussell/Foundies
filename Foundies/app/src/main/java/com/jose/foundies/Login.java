a7f440010ac868b38408ef7130e010d6a505336f
colmenerobnhs10 <jose.colmenero@tamu.edu>
2017-03-05 02:33:32 -0600

    Added functionality to Login usign SQLite

--- Foundies/app/src/main/java/com/jose/foundies/Login.java
+++ Foundies/app/src/main/java/com/jose/foundies/Login.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    DatabaseHelper helper = new DatabaseHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +20,25 @@ public class Login extends AppCompatActivity {
 
 
         final Button login = (Button) findViewById(R.id.login_button);
-
+        final EditText email = (EditText) findViewById(R.id.email_field);
+        final EditText password = (EditText) findViewById(R.id.password_field);
 
         login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent i = new Intent(getBaseContext(), LostorFound.class);
-                startActivity(i);
-                finish();
+                if(view.getId()== R.id.login_button) {
+                    String emailstr = email.getText().toString();
+                    String passstr = password.getText().toString();
+
+
+                    String passCheck = helper.searchPass(emailstr);
+                    if (passstr.equals(passCheck)) {
+                        Intent i = new Intent(getBaseContext(), LostorFound.class);
+                        startActivity(i);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Email and Password don't match", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
