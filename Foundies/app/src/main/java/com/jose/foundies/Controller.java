9af17c15b151105da8e8a434ff834a2c25cc1086
kpreston1 <kyle.h.preston75>
2017-04-18 18:26:45 -0500

    adding users lost items functionality

--- Foundies/app/src/main/java/com/jose/foundies/Controller.java
+++ Foundies/app/src/main/java/com/jose/foundies/Controller.java
@@ -255,6 +255,9 @@ public class Controller extends Application {
 
     /* --------------------------------- Lost Controller Functionality ------------------------------------ */
 
+    public ArrayList<Item> getUsersLostItems(){
+        return lm.getItemsByUsername(um.getUserEmail());
+    }
 
 
 }
--- Foundies/app/src/main/java/com/jose/foundies/LostModel.java
+++ Foundies/app/src/main/java/com/jose/foundies/LostModel.java
@@ -2,6 +2,11 @@ package com.jose.foundies;
 
 import android.os.StrictMode;
 import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static java.lang.System.out;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,5 +26,63 @@ public class LostModel {
 
 
     public void LostItemModel(){}
+
+    public ArrayList<Item> getItemsByUsername(String username){
+
+        final HerokuService service = Utility.connectAPI();
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Call<ResponseBody> call = service.getItemsByUser(username);
+            try {
+                Response<ResponseBody> response = call.execute();
+                if (response.isSuccessful()) {
+                    String strResponseBody = response.body().string();
+                    return parseJSONUserFound(strResponseBody);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Item> parseJSONUserFound(String response_str) {
+        JSONObject jObject;
+        JSONArray jArray;
+        try {
+            jObject = new JSONObject(response_str);
+            jArray = jObject.getJSONArray("item");
+            ArrayList<Item> items = new ArrayList<Item>();
+            if(jArray.isNull(0)){
+                return null;
+            }
+            for(int i = 0; i < jArray.length(); i++) {
+                JSONObject curr = new JSONObject(jArray.getString(i));
+                Item item = new Item();
+                if (curr.getString("_id").isEmpty()) {
+                    return null;
+                } else {
+                    item.setItemID(curr.getString("_id"));
+                    item.setUserID(curr.getString("username"));
+                    item.setCategory(curr.getString("category1"));
+                    item.setSubcategory(curr.getString("category2"));
+                    item.setAnswers(curr.getString("category3"));
+                    item.setLatitude(curr.getString("latitude"));
+                    item.setLongitude(curr.getString("longitude"));
+                    item.setTimestamp(curr.getString("timestamp"));
+                    items.add(item);
+                }
+            }
+            return items;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
 
 }
