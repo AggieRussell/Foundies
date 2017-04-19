469c8f001daf84e9646a5a149810fd5eb4554b7a
Christine Russell <cmrussell@tamu.edu>
2017-04-19 02:14:43 -0500

    fixed aesthetic issue with spinners

--- Foundies/app/src/main/java/com/jose/foundies/AdditionalDetails.java
+++ Foundies/app/src/main/java/com/jose/foundies/AdditionalDetails.java
@@ -2,6 +2,7 @@ package com.jose.foundies;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
